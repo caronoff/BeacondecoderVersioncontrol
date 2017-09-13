445208839876fc25bd7c61a41d3b610c9a5e4cb4
Craig Aronoff <caronoff@gmail.com>
2017-09-13 15:32:46 -0400

    memo change

--- setupgentest.py
+++ setupgentest.py
@@ -3,16 +3,16 @@ from cx_Freeze import setup, Executable
 includefiles =["Countries.csv"]
 # Dependencies are automatically detected, but it might need fine tuning.
 
-build_exe_options = {"includes" : [ "re", "atexit" ],"packages": ["os"], "excludes": ["tkinter"],'include_files':includefiles,'build_exe':'decode'}
+build_exe_options = {"includes" : [ "re", "atexit" ],"packages": ["os"], "excludes": ["tkinter"],'include_files':includefiles,'build_exe':'gentest'}
 
 # GUI applications require a different base on Windows (the default is for a
 # console application).
 base = None
-if sys.platform == "win32":
-    base = "Win32GUI"
+# if sys.platform == "win32":
+#    base = "Win32GUI"
 
-setup(  name = 'decoder',
+setup(  name = 'genhex',
         version = "0.1",
-        description = "Beacon Hexidecimal Decode",
+        description = "Test produce Second Generation Hex",
         options = {"build_exe": build_exe_options},
-        executables = [Executable('decoder.py', base=base)])
+        executables = [Executable('Gen2testprogram.py', base=base)])

99d62548d74e8abce04040ed31e361bb7479eac8
Craig Aronoff <caronoff@gmail.com>
2017-10-20 13:28:29 -0400

    changes

--- website/flaskproj/__init__.py
+++ website/flaskproj/__init__.py
@@ -1,31 +1,59 @@
 from flask import Flask, jsonify,request, render_template, Markup
-
-
+import re
+import decodehex2
+import definitions
+import sys
+import Gen2secondgen as Gen2
+import Gen2functions
 app = Flask(__name__)
 
 
 
 @app.route('/validatehex', methods=['GET','POST'])
 def validatehex():
-    ret_data =  str(request.args.get('hexcode'))
-    if ret_data == 'craig':
-        statuscheck='valid'
-    else:
-        statuscheck='not valid'
-    return jsonify(echostatus=statuscheck)
+    ret_data =  str(request.args.get('hexcode')).strip()
+    hexaPattern = re.findall(r'([A-F0-9])', ret_data,re.M|re.I)
+    statuscheck='not valid'
+    message = 'Enter a valid beacon hex message'
+    if len(ret_data) > 0:
+        if len(hexaPattern)==len(ret_data):
+
+            message='This is a valid hexidecimal message.'
+            if len(ret_data) in [15,22,30,23,63]:
+                statuscheck = 'valid'
+            else:
+                message += '  However, length '+str(len(ret_data)) + ' is not a valid message.  Valid message should be 15,22,23,30 or 63.'
+        else:
+            statuscheck='not valid'
+            message='Invalid Hexidecimal code.  (A-F-0-9)'
+
+
+    return jsonify(echostatus=statuscheck, message=message)
 
 @app.route('/output', methods=['GET','POST'])
 def output():
     ## output will only process if hexcode has been validated.
     outputdata="dummy"
-    print(request.method)
     if request.method== 'GET':
-        outputdata= str(request.args.get('hexcode'))
+        hexcode= str(request.args.get('hexcode'))
         username = str(request.args.get('username'))
+    decoded = [[hexcode,username,'hello','stuf'],['abc','123','bye','blah']]
 
 
 
-    return render_template('output.html',hexcode=outputdata, username=username)
+    if len(hexcode) == 63 or len(hexcode) == 51 or len(hexcode) == 75 or len(hexcode) == 23:
+        beacon = Gen2.SecondGen(hexcode)
+    else:
+        beacon = decodehex2.BeaconHex(hexcode)
+    print(hexcode)
+
+    beacon.processHex(hexcode)
+    ctry = beacon.country()
+    mid = str(ctry[0][1])
+    name = str(ctry[1][1])
+    print(name)
+
+    return render_template('output.html',hexcode=hexcode,decoded=beacon.tablebin)
 
 @app.route("/")
 @app.route("/index")
